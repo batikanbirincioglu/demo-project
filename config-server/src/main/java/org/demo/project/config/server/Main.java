4365e066fd136a688fa26a7ae00d06add22c2630
batikanbirincioglu <batikanbirincioglu@gmail.com>
2020-10-27 10:49:46 +0300

    demo-project

--- config-server/src/main/java/org/demo/project/config/server/Main.java
+++ config-server/src/main/java/org/demo/project/config/server/Main.java
@@ -8,6 +8,6 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-         SpringApplication.run(Main.class, args);
+        SpringApplication.run(Main.class, args);
     }
 }
